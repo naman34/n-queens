ed2ee7f16b97390502385383963722cba73610bf
Naman Goel <naman34@gmail.com>
2014-03-06 17:33:11 -0800

    bitwise

--- src/worker.js
+++ src/worker.js
@@ -1,3 +1,44 @@
+
+
+
+
+
+
 self.addEventListener('message', function(e) {
+
+  var count = 0;
+
+  var bitwiseQueens = function(col, minD, majD, row, n){
+
+    for (var i = 0; i < n; i++) {
+      var attempt = 1 << i;
+      //var newcols = col | attempt;
+      if( ( (col | minD | majD) & attempt) !== attempt){
+
+        if(row === n-1){
+          count ++;
+        } else {
+
+          bitwiseQueens(
+            col | attempt,
+            (minD | attempt) << 1,
+            (majD | attempt) >> 1,
+            row +1,
+            n
+          );
+
+        }
+
+      }
+
+    }
+
+  };
+
+
+  //var c = e.data.col;
+
+  bitwiseQueens(...);
+
   self.postMessage(e.data);
 }, false);
